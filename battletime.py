5008b0534a474d57bc79570c5480f372d0dc6f8b
unknown <pedelgadillo@gmail.com>
2021-10-08 15:53:43 -0500

    Finishing up the comparison between the two players gestures added a display winners function

--- battletime.py
+++ battletime.py
@@ -6,17 +6,9 @@ class Game:
     def __init__(self):
         self.player_one = Human()
         self.player_two = None
-        self.ai = AI()
         self.round_count = 1
         
-    def round_outcome(self):
-        if self.player_one.choice == self.player_two.choice:
-            print("Both players selected {self.player_one.choice}. You tied.")
-        elif self.player_one.choice == "rock":
-            if self.player_two.choice == "scissors":
-                print("Rock crushes scissors {self.player_one} wins this round")
-                self.player_one.score += 1
-
+    
     def display_rule(self):
         print(
             """Rule #1: Select a gesture and battle against your opponet"""
@@ -26,25 +18,57 @@ class Game:
             """Rule #3: No points for ties""")
     
     def select_game_mode(self):
-        game_mode_one = "Human vs Human"
+        game_mode_one = "Human vs Human" #validation
         game_mode_two = "Human vs AI"
         select = input("Which game mode would you like to play? ")
         print("Human vs Human \n Human vs AI ")
         if select == game_mode_one:
-            self.human_vs_human
+            self.player_two = Human()
         elif select == game_mode_two:
-            self.human_vs_AI
+            self.player_two = AI()
             
-    def human_vs_human(self, player_one, player_two):
-        self.goes_first()
-        play = True
-        while self.round_count <= 0 :
-            pass
-
-    def human_vs_AI(self, player_one, ai):
-        self.goes_first()
-    
     
     def goes_first(self, player_one, player_two):
         goes_first = random.choice(player_one, player_two)
         return goes_first
+    
+    def round_outcome(self):
+        if self.player_one.choice == self.player_two.choice:
+            print(f"Both players selected {self.player_one.choice}. You tied.")
+        elif self.player_one.choice == "rock":
+            if self.player_two.choice == "scissors":
+                print(f"Rock crushes scissors {self.player_one} wins this round")
+                self.player_one.score += 1
+            elif self.player_two.choice == "lizard":
+                print(f"Rock crushes lizard! {self.player_one.name} wins this round!")
+                self.player_one.score += 1
+            elif self.player_two.choice == "spock":
+                print(f"Spock vaporizes rock! {self.player_two.name} wins this round.")
+                self.player_two.score += 1
+            elif self.player_two.choice == "paper":
+                print(f"Paper covers rock! {self.player_two.name} wins this round.") 
+                self.player_two.score += 1   
+        elif self.player_one.choice == "paper":
+            if self.player_two.choice == "rock":
+                print(f"Paper covers rock! {self.player_one.name} wins this round!")
+                self.player_one.score += 1
+            elif self.player_two.choice == "spock":
+                print(f"Paper disproves spock! {self.player_one.name} wins this round!")
+                self.player_one.score += 1
+            elif self.player_two.choice == "lizard":
+                print(f"Lizard eats paper! {self.player_two.name} wins this round.")
+                self.player_two.score += 1
+            elif self.player_two.choice == "scissors":
+                print(f"Scissors cuts paper! {self.player_two.name} wins this round.")
+                self.player_two.score += 1
+
+
+
+
+
+
+    def display_winners(self):
+        if (self.player_one.score == 2):
+            print(f"{self.player_one.name} wins!")
+        elif (self.player_two.score == 2):
+            print(f"{self.player_two.name} wins!")             
