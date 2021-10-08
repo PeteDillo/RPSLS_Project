0b3d4d79f0fa67c6206bcbea67a5026177735fee
unknown <pedelgadillo@gmail.com>
2021-10-08 14:41:06 -0500

    Merge

--- battletime.py
+++ battletime.py
@@ -6,3 +6,10 @@ class Game:
         self.player_two = None
         self.round_count = 1
         
+    def round_outcome(self):
+        if self.player_one.choice == self.player_two.choice:
+            print("Both players selected {self.player_one.choice}. You tied.")
+        elif self.player_one.choice == "rock":
+            if self.player_two.choice == "scissors":
+                print("Rock crushes scissors {self.player_one} wins this round")
+                self.player_one.score += 1
