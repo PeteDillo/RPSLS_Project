eb936261f19cee80dd67bcb6e48b6eb0dc241706
JESSE ARROYO-ROMERO <jesseromero310@gmail.com>
2021-10-08 14:01:29 -0500

    Created function for AI class to randomly select one from the gesture list.

--- AI.py
+++ AI.py
@@ -1,6 +1,11 @@
 from player import Player
+import random
 
 class AI(Player):
     def __init__(self):
+        self.name = 'AI'
         super().__init__()
         pass
+
+    def gesture_select(self, gesture_list):
+        self.choice = random.choice(gesture_list)
--- battletime.py
+++ battletime.py
@@ -1,3 +1,6 @@
 class Battletime:
     def __init__(self):
         pass
+
+
+    
